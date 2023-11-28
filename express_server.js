9294208376d1b95af9ae54f5cbc5208d91885e7f
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-27 23:19:56 -0600

    added post route for form/ parsed post-request body/ rendered form

--- express_server.js
+++ express_server.js
@@ -4,10 +4,19 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const generateRandomString = function() {
+
+};
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//body parser
+app.use(express.urlencoded({ extended: true }));  //data submitted using post request is in non human readable form, converts buffer into string.
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,9 +34,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
